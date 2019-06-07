41ae6779d86004c1e3240d6d05616526e02421ae
Oakkar <oakkaraung1659@gmail.com>
2019-06-07 12:55:16 -0700

    added parallax effect to home page

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function() {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * -0.5 + "px";
+})
+
+$("#searchBtn").on("click", function(){
+    var input = $("#city-input").val().trim();
+    $("#city-name").html("<h1>" + input + "</h1>");
+});

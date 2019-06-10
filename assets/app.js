03a7f6e5e257d20c2124d1f803b3f969bd7e9886
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-10 11:33:18 -0700

    results from input now show an image related to the search in background

--- assets/app.js
+++ assets/app.js
@@ -1,52 +1,60 @@
 
-const parallax  = document.getElementById("parallax");
+const parallax = document.getElementById("parallax");
 
 window.addEventListener("scroll", function () {
-let offset = window.pageYOffset;
+    let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * -0.5 + "px";
 })
 
-$("#searchBtn") .on("click", function () {
+var data;
+var hits;
+
+$("#searchBtn").on("click", function () {
     var input = $("#city-input").val().trim();
-        $("#city-name").html("<h1>" + input + "</h1>");
+    $("#city-name").html("<h1>" + input + "</h1>");
 
     var API_KEY = '12715505-5709c2fd53c134eb9abea5b53';
-        var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(input);
-        $.getJSON(URL, function (data) {
-            var hits = data.hits
-            for (i = 0; i < hits.length; i++) {
-                console.log(data.hits[i].largeImageURL);
-            }
+    var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(input);
+    $.getJSON(URL, function (data) {
+        hits = data.hits;
+        for (i = 0; i < hits.length; i++) {
+            console.log(data.hits[i].largeImageURL);
+           
+        }
 
-        });
-
-
-        var headline = $("<div>");
-        headline.css({
-            'float': 'screenLeft',
-            'border': '1px solid black'
-        })
-        var qUrl = 'https://newsapi.org/v2/everything?q=' + input + '&apiKey=ba75c605a3c141558186bc2db4f3dc52';
-        $.ajax({
-            url: qUrl,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            for (var i = 0; i < 5; i++) {
-                var cardThing = $("<div>").addClass("card");
-                var cardBody = cardThing.append($("<div>").addClass("card-body"));
-                cardBody.append("<h3>" + response.articles[i].title + "</h3>");
-                cardBody.append("<h6>" + response.articles[i].author + "</h6>");
-                cardBody.append($('<a href=' + response.articles[i].url + 'class="btn btn-primary">Go to Article</a>'));
-                headline.append(cardThing);
-            }
-        });
-        $("#new-city").html(headline);
-
-
-
+        $("#new-city").css("background-image", "url(" + data.hits[0].largeImageURL + ")");
 
     });
+
+
+    var headline = $("<div>");
+    headline.css({
+        'float': 'screenLeft',
+        'border': '1px solid black'
+    })
+    var qUrl = 'https://newsapi.org/v2/everything?q=' + input + '&apiKey=ba75c605a3c141558186bc2db4f3dc52';
+    $.ajax({
+        url: qUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < 5; i++) {
+            var cardThing = $("<div>").addClass("card");
+            var cardBody = cardThing.append($("<div>").addClass("card-body"));
+            cardBody.append("<h3>" + response.articles[i].title + "</h3>");
+            cardBody.append("<h6>" + response.articles[i].author + "</h6>");
+            cardBody.append($('<a href=' + response.articles[i].url + 'class="btn btn-primary">Go to Article</a>'));
+            headline.append(cardThing);
+        }
+    });
+    $("#new-city").html(headline);
+
+
+
+
+});
+
+
 
 
 

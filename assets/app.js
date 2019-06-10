8fd2ab68d5e6c32dbef238ddcc2527cd4b80a208
Max Patten <amanthus2535@gmail.com>
2019-06-10 11:47:42 -0700

    made notes to self and got per page limit up on qURL

--- assets/app.js
+++ assets/app.js
@@ -11,11 +11,15 @@ $("#searchBtn") .on("click", function () {
         $("#city-name").html("<h1>" + input + "</h1>");
 
     var API_KEY = '12715505-5709c2fd53c134eb9abea5b53';
-        var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(input);
+    //Monday - Max - added per_page parameter so pixabay returns 200 results instead of 20
+        var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(input) + "&per_page=200";
         $.getJSON(URL, function (data) {
             var hits = data.hits
             for (i = 0; i < hits.length; i++) {
-                console.log(data.hits[i].largeImageURL);
+                console.log(data.hits[i].comments);
+                //Monday - Max - adding in method to sort returned items and sort them... for now I will do this as an array of the top 20 hits by # of favorites. We can then decide whether we want to have a random single image as background or do something fancier with that array of results.
+        
+
             }
 
         });
